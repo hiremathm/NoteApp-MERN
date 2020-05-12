69cedfb6b06d0feb3a2dfdf03b18b75aa984473f
Shivakumara M <shivasorab@gmail.com>
2020-05-13 05:24:50 +0530

    mongodb setup changed

--- config/database.js
+++ config/database.js
@@ -3,9 +3,11 @@ mongoose.Promise = global.Promise
 
 const CONNECTION_URI = process.env.MONGODB_URI || "mongodb+srv://shivakumara:shiva@123@cluster0-6pive.mongodb.net/test?retryWrites=true&w=majority"
 
+// heroku config:set MONGODB_URI='urlOfYourMongoDatabase'
+
 // const CONNECTION_URI =  "mongodb://127.0.0.1:27017/note-app"
 
-mongoose.connect(CONNECTION_URI,{useNewUrlParser: true})
+mongoose.connect(CONNECTION_URI,{useNewUrlParser: true,useCreateIndex: true})
 // it returns promise object, it is asynchronous operation
     .then(()=>{
         console.log("Connected to db")
