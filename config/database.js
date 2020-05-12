2f5a8267b15411078a8da323f4012bd5e08aea15
Shivakumara M <shivasorab@gmail.com>
2020-05-13 05:13:04 +0530

    database uri changes

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-// const CONNECTION_URI = process.env.MONGODB_URI || "mongodb+srv://shivakumara:shiva@123@cluster0-6pive.mongodb.net/test?retryWrites=true&w=majority"
+const CONNECTION_URI = process.env.MONGODB_URI || "mongodb+srv://shivakumara:shiva@123@cluster0-6pive.mongodb.net/test?retryWrites=true&w=majority"
 
-mongoose.connect('mongodb://127.0.0.1:27017/note-app',{useNewUrlParser: true})
+// const CONNECTION_URI =  "mongodb://127.0.0.1:27017/note-app"
+
+mongoose.connect(CONNECTION_URI,{useNewUrlParser: true})
 // it returns promise object, it is asynchronous operation
     .then(()=>{
         console.log("Connected to db")
