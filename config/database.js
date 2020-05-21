3b0c2612b6829a04256fc141327d66f2b4489521
Shivakumara M <shivasorab@gmail.com>
2020-05-21 19:07:07 +0530

    database url changes

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const CONNECTION_URI = process.env.MONGODB_URI || "mongodb+srv://shivakumara:shiva@123@cluster0-6pive.mongodb.net/note-app?retryWrites=true&w=majority"
-// const CONNECTION_URI = "mongodb://127.0.0.1:27017/note-app"
+// const CONNECTION_URI = process.env.MONGODB_URI || "mongodb+srv://shivakumara:shiva@123@cluster0-6pive.mongodb.net/note-app?retryWrites=true&w=majority"
+const CONNECTION_URI = "mongodb+srv://shivakumara:shiva@123@cluster0-6pive.mongodb.net/note-app?retryWrites=true&w=majority"
 
 mongoose.connect(CONNECTION_URI,{useNewUrlParser: true,useCreateIndex: true})
 // it returns promise object, it is asynchronous operation
