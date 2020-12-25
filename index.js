31b24bcfe7c4f658e011a62125c08e781ee0734c
Shivakumara M <shivasorab@gmail.com>
2020-12-25 19:05:20 +0530

    server start commands changes

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ app.use(cors())
 const port = process.env.PORT || 6060; 
 // const port = 6060
 app.use(express.static(path.join(__dirname,"client/build"))) 
-// app.get("*",(req,res) => { 
+app.get("*",(req,res) => { 
     // res.sendFile(path.join(__dirname + "/client/build/index.html")) 
-// }) 
+ 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+}) 
 
 // app listening to port 
 app.listen(port, function(){
