db6964e873078ca9f559e0cf24c11754a9c4d7d5
Shivakumara M <shivasorab@gmail.com>
2020-12-25 20:19:23 +0530

    end point changed

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,24 +1,15 @@
 import Axios from 'axios'
-// var environment = process.env.NODE_ENV || 'development';
-// console.log("env", environment)
-// let axios = ''
-// if(environment === 'development'){
-//   axios = Axios.create({
-//      baseURL: "http://localhost:6060/api/"
-//   });
-// }else{
-//   axios = Axios.create({
-//     baseURL: "/api/"
-//   });
-// }
-
-// console.log("AXIOS", axios)
-
-// export default axios;
-
-
-axios = Axios.create({
-	baseURL: "http://localhost:6060/api/"
-});
+var environment = process.env.NODE_ENV || 'development';
+console.log("env", environment)
+let axios = ''
+if(environment === 'development'){
+  axios = Axios.create({
+     baseURL: "http://localhost:6060/api/"
+  });
+}else{
+  axios = Axios.create({
+    baseURL: "/api/"
+  });
+}
 
 export default axios;
