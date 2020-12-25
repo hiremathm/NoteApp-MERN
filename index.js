ce8a41de3dc73150c86ec3e93230bc3e4d0b6c32
Shivakumara M <shivasorab@gmail.com>
2020-12-25 19:37:10 +0530

    build changed

--- index.js
+++ index.js
@@ -17,24 +17,23 @@ app.use(cors())
 const port = process.env.PORT || 6060; 
 // const port = 6060
 app.use(express.static(path.join(__dirname,"client/build"))) 
-app.get("*",(req,res) => { 
+
+
+app.get("/*",(req,res) => { 
     res.sendFile(path.join(__dirname+"/client/build/index.html")) 
 }) 
+
 
 // app listening to port 
 app.listen(port, function(){
     console.log('listening to port ', port)
 })
 
-console.log('environment ', process.env.NODE_ENV)
-
 // home page
 app.get('/' , (req, res) =>{
     res.send('Welcome to noteapp app')
 })
 
-// note routes
-// app.use('/notes,notesController)
 app.use('/api/', Router)
 app.use('/api/categories', categoriesRouter)
 app.use('/api/tags', tagsRouter)
